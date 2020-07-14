a57d32bf6761c1106ae76fd8058852c5fa8f9d3b
Leegihong <dlrlghd96@hanmail.net>
2020-07-14 14:01:06 +0900

    osm file on traffic light

--- exp_configs/non_rl/osm_test.py
+++ exp_configs/non_rl/osm_test.py
@@ -10,19 +10,24 @@ from flow.core.params import NetParams
 from flow.core.params import InitialConfig
 from flow.core.params import EnvParams
 from flow.core.params import SumoParams
+from flow.core.params import TrafficLightParams
+
 # map data
 from Network.osm_network import OsmNetwork
 from flow.envs.base import Env
 import gym
 from abc import ABCMeta
+tl_logic = TrafficLightParams(baseline= False)
 net_params = NetParams(
     osm_path='./Network/map.osm'
+
 )
 env_params = EnvParams()
 sim_params = SumoParams(render=True)
 initial_config = InitialConfig()
 vehicles = VehicleParams()
-vehicles.add('human', num_vehicles=1,)
+vehicles.add('human', num_vehicles=80,)
+ 
 
 flow_params = dict(
     exp_tag='osm_test',
@@ -34,6 +39,7 @@ flow_params = dict(
     net=net_params,
     veh=vehicles,
     initial=initial_config,
+    tls = tl_logic
 )
 
 # Env에서 TestEnv 는 baseEnv를 부르고 base_env에서 traci network도 호출 # 거기서 이제 맵데이터를 꺼내올 수 있음
