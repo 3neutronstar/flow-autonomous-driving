78336cfb4abafbf9c4b6ef43063a69d1dfd6564d
MinsooKang <3neutronstar@gmail.com>
2020-08-21 10:47:45 +0900

    bug fix

--- train_rllib.py
+++ train_rllib.py
@@ -102,19 +102,19 @@ def setup_exps_rllib(flow_params,
         alg_run = "DDPG"
         agent_cls = get_agent_class(alg_run)
         config = deepcopy(agent_cls._default_config)
-        config['n_step'] = 1
+        config['n_step'] = 2
         config["num_workers"] = 1
         # model
         config['actor_hiddens'] = [64, 64]
         config['actor_lr'] = 0.0001  # in article 'ddpg'
-        config['critic_lr'] = 0.001
+        config['critic_lr'] = 0.0005
         config['critic_hiddens'] = [64, 64]
         config['gamma'] = 0.99
         config['model']['fcnet_hiddens'] = [64, 64]
         config['lr']=1e-4
         # exploration
         config['exploration_config']['final_scale'] = 0.02
-        config['exploration_config']['scale_timesteps'] = 100000
+        config['exploration_config']['scale_timesteps'] = 540000
         config['exploration_config']['ou_base_scale'] = 0.1
         config['exploration_config']['ou_theta'] = 0.15
         config['exploration_config']['ou_sigma'] = 0.2
@@ -122,7 +122,7 @@ def setup_exps_rllib(flow_params,
         config['tau'] = 0.002
         config['l2_reg'] = 1e-6
         config['train_batch_size'] = 64
-        config['learning_starts'] = 3000
+        config['learning_starts'] = 1500
         # evaluation
         #config['evaluation_interval'] = 5
         config['buffer_size'] = 50000
