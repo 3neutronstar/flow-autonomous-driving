17066a559a601cc4b28f7dd966011719c42729e4
MinsooKang <3neutronstar@gmail.com>
2020-09-02 16:56:37 +0900

    remove private file

--- train_rllib.py
+++ train_rllib.py
@@ -136,7 +136,7 @@ def setup_exps_rllib(flow_params,
             config['n_step'] = 1
             config['actor_hiddens'] = [64, 64]
             config['actor_lr'] = 0.0001  # in article 'ddpg'
-            config['critic_lr'] = 0.001
+            config['critic_lr'] = 0.0001
             config['critic_hiddens'] = [64, 64]
             config['gamma'] = 0.99
             config['model']['fcnet_hiddens'] = [64, 64]
